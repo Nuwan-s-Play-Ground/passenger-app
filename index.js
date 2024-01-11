19ce0a2369e805872f445a7721f6bbdaf32809d5
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-11 22:52:01 +0530

    Implement save function

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let countEl = document.getElementById("count-el");
 
 function increment() {
     count += 1;
-    countEl.textContent = count;
+    countEl.textContent=count;
 }
 
 function save() {
     let countStr = count + " - ";
-    saveEl.textContenttStr;
-    countEl.textContent = 0;
-    count = 0;
+    saveEl.textContent += countStr;
+   // countEl.textContent = 0;
+   // count = 0;
 }
 
 console.log("Let's count people on the subway!");
